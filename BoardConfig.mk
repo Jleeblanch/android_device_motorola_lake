b5af15dc0570627d6ed6532b7efee0945350b957
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-12-14 21:11:37 -0500

    lake: Set force_normal_boot=1 on kernel command line
    Bootloader is required to pass this parameter to the kernel command line,
    however this isn't possible without access to bootloader source, so
    instead just set it ourselves.
    
    Change-Id: I12fe19c9866beb7318ac898ea51a2070c3b006b1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ SOONG_CONFIG_MOTOROLA_SDM660_INIT_DEVICE_LIB := //$(DEVICE_PATH):libinit_lake
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 1
+BOARD_KERNEL_CMDLINE += androidboot.force_normal_boot=1
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/c0c4000.sdhci
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
